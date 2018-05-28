dc7150a2df92dbd1fe19d13b71bb8b7ac53a5673
Adace Silviu <adacesilviu@yahoo.ro>
2018-05-28 17:16:36 +0200

    Forgot to add folder

---
+++ VIAFitness2/App_Data/dbo.Table_1.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[user]
+(
+	[IdUser] INT NOT NULL PRIMARY KEY, 
+    [Password] VARCHAR(50) NOT NULL, 
+    [Name] VARCHAR(50) NOT NULL, 
+    [SignDate] DATE NOT NULL
+)
---
+++ VIAFitness2/App_Data/dbo.Table_3.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Monthly]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Summary] VARCHAR(50) NULL, 
+    [GraphViews] VARCHAR(50) NULL, 
+    [IdUser] NCHAR(10) NULL
+)
